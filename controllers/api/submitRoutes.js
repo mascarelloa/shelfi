aef1c99805911920cb6aa3dfc3ae2296a3dba6af
mascarelloa <amfm0793@gmail.com>
2021-05-04 14:25:09 -0400

    started routes, started css, removed unecessary files.

---
+++ controllers/api/submitRoutes.js
@@ -0,0 +1,12 @@
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newProject = await Project.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
