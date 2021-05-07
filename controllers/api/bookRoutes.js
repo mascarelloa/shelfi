302148487a94a19c4ed61399942720e96f6f9aef
mascarelloa <amfm0793@gmail.com>
2021-05-07 15:00:57 -0400

    added sequelize operator

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -1,58 +1,55 @@
-const router = require('express').Router();
-const Book = require('../../models/book');
+const router = require("express").Router();
+const Book = require("../../models/book");
+const { Op } = require("sequelize");
 
 // This gets all books.
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Book.findAll().then((bookData) => {
     res.json(bookData);
   });
 });
 
-// This get all books by the queried author. 
-router.get('/author/:author', (req, res) => {
+// This get all books by the queried author.
+router.get("/author/:author", (req, res) => {
   Book.findAll({
     where: {
-      //use operator to pull like data to input.
-      authors: req.params.author
-    }
+      // use operator to pull like data to input.
+
+      authors: {
+        [Op.substring]: "%" + req.params.author + "%",
+      },
+    },
   }).then((bookData) => {
     //This loops through the data to create an array.
-    const books = bookData.map((book) =>
-      book.get({ plain: true })
-    );
+    const books = bookData.map((book) => book.get({ plain: true }));
     //This renders the data to the author-search view.
-    res.render("author-search",  {layout: "second.handlebars", books});
+    res.render("author-search", { layout: "second.handlebars", books });
   });
 });
 
 // This gets all books by the queried title.
-router.get('/title/:title', (req, res) => {
+router.get("/title/:title", (req, res) => {
   Book.findAll({
     where: {
-      title: req.params.title
-    }
+      title: { [Op.substring]: "%" + req.params.title + "%" },
+    },
   }).then((bookData) => {
-    const books = bookData.map((books) =>
-    books.get({plain:true}) );
+    const books = bookData.map((books) => books.get({ plain: true }));
 
-    res.render("title-search",  {layout: "second.handlebars", books});
+    res.render("title-search", { layout: "second.handlebars", books });
   });
 });
 
-
-  // This gets all books by the queried genre.
-router.get('/genre/:category', (req, res) => {
+// This gets all books by the queried genre.
+router.get("/genre/:category", (req, res) => {
   Book.findAll({
     where: {
-      categories: req.params.category
-    }
+      categories: req.params.category,
+    },
   }).then((bookData) => {
     res.json(bookData);
   });
 });
-
-
-
 
 // This will add a new book to the database.
 // router.post('/', (req, res) => {
@@ -64,6 +61,5 @@ router.get('/genre/:category', (req, res) => {
 //       res.json(err);
 //     });
 // });
-
 
 module.exports = router;
