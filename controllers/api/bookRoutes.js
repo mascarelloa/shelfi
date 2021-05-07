8bf866cfba9c6b5f44d6ae11b658fee7e3c8f498
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-05-06 20:52:22 -0400

    just did added the code on the controllers that i was showing to you and john

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -1,13 +1,46 @@
 const router = require('express').Router();
-const { Book } = require('../../models');
+const Book = require('../../models/book');
 
-//This will get or find all books from the database by the queried title.
-router.get("/:query", (req, res) => {
-    Book.findAll ({where: {title: req.params.query}})
-    .then(data) 
-})
- 
+// GET all books
+router.get('/', (req, res) => {
+  // Get all books from the book table
+  Book.findAll().then((bookData) => {
+    res.json(bookData);
+  });
+});
 
+// The route to query all data related to specfically to the author 
+router.get('/:author', (req, res) => {
+  console.log (req.params.author)
+  Book.findAll({
+    where: {
+      authors: req.params.author
+    }
+  }).then((bookData) => {
+    res.json(bookData);
+  });
+});
+
+
+// GET a single book based on their id #
+router.get('/:id', (req, res) => {
+  // Find one of the books by its primary key (book_id)
+  Book.findByPk(req.params.id)
+  .then((bookData) => {
+    res.json(bookData);
+  });
+});
+
+// CREATE a book
+router.post('/', (req, res) => {
+  Book.create(req.body)
+    .then((newBook) => {
+      res.json(newBook);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 
 module.exports = router;
