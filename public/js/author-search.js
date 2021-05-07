7cf36d54c054739eea6fd69d8bebccee561c638c
mascarelloa <amfm0793@gmail.com>
2021-05-07 14:10:25 -0400

    updating css

--- public/js/author-search.js
+++ public/js/author-search.js
@@ -1,17 +1,5 @@
-
-// //This will take the search value and add it to the endpoint.
-// // const searchInput = (searchValue) => {
-// //     console.log(searchValue)
-//     (`/api/books/author/${searchValue}`)
-//     // .then(() => location.replace("/authorsearch"))
-//     }
-  
-
-
 //This will grab the value of the search input when the search button is clicked and run the function to get the route to get the data.
 document.querySelector("#search-button").addEventListener("click", function() {
     const searchValue = document.getElementById("author-search").value;
     location.href="/api/books/author/" + searchValue;
-    // console.log(searchValue);
-    // searchInput(searchValue);
 }) 
